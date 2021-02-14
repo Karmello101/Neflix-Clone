d5a0380fe35a69999359996d5310d86ad3ba14aa
Karmello101 <kevinarmella101@gmail.com>
2021-02-13 21:40:40 -0500

    img added withou css

---
+++ src/request.js
@@ -0,0 +1,26 @@
+const API_KEY = "6e65e5a5e366967973b9ea93135edcb7";
+
+const requests = {
+    fetchTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`,
+    fetchNeflixOriginals:`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US`,
+    fetchTopRatedMovies:`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`https://api.themoviedb.org/3/discover/movie?api_key${API_KEY}&with_genres=99`
+}
+
+export default requests;
+
+///discover/tv?api_key=${API_KEY}&with_networks=213
+// fetchTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`,
+// fetchTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`,
+// fetchNeflixOriginals:`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+// fetchTopRatedMovies:`/movie/top/rated?api_key=${API_KEY}&language=en-US`,
+// fetchActionMovies:`/discover/movie?api_key${API_KEY}&with_genres=28`,
+// fetchComedyMovies:`/discover/movie?api_key${API_KEY}&with_genres=35`,
+// fetchHorrorMovies:`/discover/movie?api_key${API_KEY}&with_genres=27`,
+// fetchRomanceMovies:`/discover/movie?api_key${API_KEY}&with_genres=10749`,
+// fetchDocumentaries:`/discover/movie?api_key${API_KEY}&with_genres=99`
+// }
