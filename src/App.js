f4b8bb3f573c95bcec0455a3bead346396e2eb2d
Karmello101 <kevinarmella101@gmail.com>
2021-02-13 22:50:10 -0500

    added css to netflix original row

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello All </h1>
-      <Row title="NETFLIX ORIGINALS"  fetchUrl={requests.fetchNeflixOriginals}/>
+      <Row title="NETFLIX ORIGINALS"
+           fetchUrl={requests.fetchNeflixOriginals}
+           isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
